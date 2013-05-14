8aaff3a3b32e8b690bfbd63e610806bc13822d13
Sachiano <sacha.corazzi@gmail.com>
2013-05-14 17:46:37 +0100

    Test mail after text

--- apis/twilio/sms.php
+++ apis/twilio/sms.php
@@ -1,6 +1,5 @@
 <?php
-$myFile = "testFile.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
+
 $stringData = print_r($_POST);
-fwrite($fh, $stringData);
-fclose($fh);
+
+mail("sacha.corazzi@gmail.com","Text",$stringData);
